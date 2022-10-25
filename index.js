4864a990c1265cf9972c0661fd2e52c711c26a10
kuronight <85890469+kuronight29@users.noreply.github.com>
2022-10-25 23:23:49 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const { Client } = require('discord.js-selfbot-v13');
+const { token } = require('./config.json');
+const { joinVoiceChannel } = require('@discordjs/voice');
+const client = new Client(); 
+
+client.on('ready', async () => {
+  console.log(`${client.user.username} is ready!`);
+  client.channels.fetch("935924655951347712") 
+    .then((channel) => { 
+        const VoiceConnection = joinVoiceChannel({
+            channelId: "935924655951347712", 
+            guildId: "755793441287438469", 
+            adapterCreator: channel.guild.voiceAdapterCreator 
+        });
+    });
+})
+
+client.login(token);
